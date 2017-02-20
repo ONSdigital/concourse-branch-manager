415681173ae94a4702a9171433d5b1279004891d
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2017-02-20 07:42:03 +0000

    install version 0.0.3 of process_helper

--- tasks/core/setup_process_helper.rb
+++ tasks/core/setup_process_helper.rb
@@ -1,4 +1,4 @@
-system('gem install process_helper --no-ri --no-rdoc') ||
+system('gem install process_helper --version 0.0.3 --no-ri --no-rdoc') ||
   fail('failed to install process_helper gem')
 require 'rubygems'
 require 'process_helper'
